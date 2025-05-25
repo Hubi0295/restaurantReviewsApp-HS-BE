337ce43afb86edd1efb5fb723089027c8324edf1
hubert <hubert.szelepusta@gmail.com>
2025-05-25 18:38:13 +0200

    Dodano funkcjonalność dodawania recenzji restauracji oraz stworzono endpoint do wysylania danych o recenzjach uzytkownika

--- models/Review.js
+++ models/Review.js
@@ -1,17 +1,14 @@
 const mongoose = require("mongoose");
 const { Schema, Types } = mongoose;
 
-const dishSchema = new Schema({
-    meal: { type: String, required: true },
-    review: { type: String, required: true }
-});
 
 const reviewSchema = new Schema({
     user: { type: Types.ObjectId, ref: "User", required: true },
     restaurant: { type: Types.ObjectId, ref: "Restaurant", required: true },
     date: { type: Date, required: true, default: Date.now },
     review: { type: String, required: true },
-    dishes: [dishSchema],
+    dishes: [{ type: String, required: true }],
+    images: [{ type: String, required: true }],
     rating: { type: Number, min: 1, max: 10 }
 });
 const Review = mongoose.model("Review", reviewSchema);
--- routes/api.js
+++ routes/api.js
@@ -8,40 +8,76 @@ const { User } = require("../models/User");
 const jwtSecret = process.env.JWT_SECRET;
 const multer = require("multer");
 const path = require('path');
-const storage = multer.diskStorage({
+const storageReview = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "uploads/");
+        cb(null, "uploads/reviews/");
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname));
     }
 });
-const upload = multer({ storage });
-
-router.post("/review", upload.any(), async (req, res) => {
-    try {
-        const { restaurantName, review, rating } = req.body;
-        const uploadedFiles = req.files;
-        const dishes = [];
-        uploadedFiles.forEach((file) => {
-            const indexMatch = file.fieldname.match(/dishes\[(\d+)\]\[image\]/);
-            if (indexMatch) {
-                const index = indexMatch[1];
-                const nameKey = `dishes[${index}][name]`;
-                const dishName = req.body[nameKey];  // <-- poprawnie odczytany
-                dishes.push({
-                    name: dishName,
-                    image: file.filename
-                });
-            }
-        });
-        console.log("Dishes:", dishes);
-        res.status(200).json({ message: "Recenzja zapisana.", dishes });
-    } catch (e) {
-        console.error(e);
-        res.status(500).json({ message: "Błąd serwera." });
+const uploadReview = multer({ storage: storageReview });
+const storageRestaurant = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/restaurants/");
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
     }
 });
+const uploadRestaurant = multer({ storage: storageRestaurant });
+router.post("/review", uploadReview.any(), async (req, res) => {
+    try{
+        const {restaurantName, review, rating, dishes} = req.body;
+        const imagesUrl = [];
+
+        const token = req.cookies.token;
+        const decoded = jwt.verify(token, jwtSecret);
+        const userID = await User.find({_id: decoded._id},{_id:1});
+        const restaurantID = await Restaurant.find({name: req.body.restaurantName},{_id:1});
+
+
+        req.files.forEach(file => {
+            imagesUrl.push(file.filename);
+        });
+        console.log(userID[0]._id)
+        console.log(restaurantID[0]._id)
+        console.log(restaurantName)
+        console.log(review)
+        console.log(rating)
+        console.log(dishes)
+        console.log(imagesUrl);
+        const newReview = new Review({
+            user: userID[0]._id,
+            restaurant: restaurantID[0]._id,
+            review,
+            dishes,
+            images: imagesUrl,
+            rating
+        });
+
+        await newReview.save();
+            res.status(201).json({message:"Dodano recenzje"});
+        }
+    catch(e){
+        console.log(e);
+        res.status(500).json({message:"Blad dodawania recenzji"});
+    }
+});
+router.get("/review",async (req,res)=>{
+    try{
+        const token = req.cookies.token;
+        const decodedID = jwt.verify(token,jwtSecret)._id;
+        const userID = await User.find({_id:decodedID});
+        const reviews = await Review.find({user:userID[0]._id});
+        res.status(200).json({data: reviews});
+    }
+    catch(e){
+        console.log(e)
+        res.status(500).json({message:"Blad serwera"});
+    }
+
+})
 
 router.get("/listRestaurant",async (req,res)=>{
     try{
@@ -53,7 +89,7 @@ router.get("/listRestaurant",async (req,res)=>{
     }
 })
 
-router.post("/restaurant", upload.single("image"), async (req, res) => {
+router.post("/restaurant", uploadRestaurant.single("image"), async (req, res) => {
     try {
         const { name, address, description, type, hasDelivery } = req.body;
         const imagePath = req.file ? req.file.filename : null;
